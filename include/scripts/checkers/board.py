b98339581c79bfb14db8346efe46485f42178a0a
AdrianZajaczkowski <63166407+AdrianZajaczkowski@users.noreply.github.com>
2021-05-20 18:22:42 +0200

    checkers game without moving

---
+++ include/scripts/checkers/board.py
@@ -0,0 +1,40 @@
+import pygame
+from .constants import COLORS, ROWS, COLS, SQUARE_SIZE , SQUARE_SIZE
+from .piece import Piece
+
+class Board():
+
+    def __init__(self):
+        self.board = []
+        self.selected_piece = None
+        self.red_left = self.white_left = 12
+        self.red_kings = self.white_kings = 0
+        self.create_board() # automaticly call method to create board
+    def draw_squares(self,win):
+        win.fill(COLORS['RED'])
+        for row in range(ROWS):
+            for col in range(row % 2, COLS,2):
+                pygame.draw.rect(win,COLORS['GREY'], (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+    
+    def create_board(self):
+        for row in range(ROWS):
+            self.board.append([])
+            for col in range(COLS):
+                if col %2 == ((row + 1 ) %2 ):
+                    if row < 3:
+                        self.board[row].append(Piece(row,col,COLORS['WHITE'])) # append pieces with COLORS on row&col
+                    elif row > 4:
+                        self.board[row].append(Piece(row,col,COLORS['BLACK']))
+                    else:
+                        self.board[row].append(0)
+                else:
+                    self.board[row].append(0)
+
+    def draw(self,win):
+        self.draw_squares(win)
+        for row in range(ROWS):
+            for col in range(COLS):
+                piece = self.board[row][col]
+                if piece != 0 :
+                    piece.draw(win)
+                    
---
+++ include/scripts/main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import pygame
+from checkers.constants import WIDTH, HEIGHT
+from checkers.board import Board
+FPS = 60
+WIN = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption('Checkers')
+###
+def main():
+    run = True
+    clock = pygame.time.Clock()
+    board = Board()
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get(): # check what event is arledy 
+            if event.type == pygame.QUIT: # event for quit loop while you press 'x'
+                run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pass
+
+        board.draw(WIN)
+        pygame.display.update()
+
+    pygame.quit() # end game
+
+main()
